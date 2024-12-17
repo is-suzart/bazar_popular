9a913da5c8608c23f8f56fba0daba6342ad51edb
Igor Suzart <igorarsuzart@gmail.com>
2024-12-17 16:54:42 -0300

    configurando toolbar do text-editor

--- lib/_pages/product/upload_product.dart
+++ lib/_pages/product/upload_product.dart
@@ -4,7 +4,6 @@ import 'package:flutter_quill/flutter_quill.dart';
 class UploadProduct extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => UploadState();
-  
 }
 
 class UploadState extends State<UploadProduct> {
@@ -15,24 +14,41 @@ class UploadState extends State<UploadProduct> {
     // TODO: implement build
     return Column(
       children: [
-
-        QuillSimpleToolbar(
-  controller: _controller,
-  configurations: const QuillSimpleToolbarConfigurations(),
-),
-const Divider(),
-  QuillEditor.basic(
-    controller: _controller,
-    configurations: const QuillEditorConfigurations(),
-  ),
-const Divider(),
-ElevatedButton(onPressed: () {
-  print(_controller.document.toDelta());
-  setState(() {
-    _p = _controller.document.toPlainText();
-  });
-}, child: Text("veja e verá")),
-Text(_p),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: QuillSimpleToolbar(
+            controller: _controller,
+            configurations: const QuillSimpleToolbarConfigurations(
+                showBackgroundColorButton: false,
+                showCodeBlock: false,
+                showColorButton: false,
+                showFontFamily: false,
+                showFontSize: false,
+                showInlineCode: false,
+                showSubscript: false,
+                showSuperscript: false,
+                showClearFormat: false,
+                showSearchButton: false,
+                showStrikeThrough: false,
+                showIndent: false,
+                headerStyleType: HeaderStyleType.buttons),
+          ),
+        ),
+        const Divider(),
+        QuillEditor.basic(
+          controller: _controller,
+          configurations: const QuillEditorConfigurations(),
+        ),
+        const Divider(),
+        ElevatedButton(
+            onPressed: () {
+              print(_controller.document.toDelta());
+              setState(() {
+                _p = _controller.document.toPlainText();
+              });
+            },
+            child: Text("veja e verá")),
+        Text(_p),
       ],
     );
   }
